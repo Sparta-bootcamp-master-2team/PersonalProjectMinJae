37ebead2a230bebfa4b8ad6b11460d4e2015dab9
minjae <qlqlzbto@naver.com>
2025-04-17 18:06:07 +0900

    feat: CalculatorVC에 CalculatorView 추가, 레이아웃 구성 및 바인딩

--- ExchangeCalculateApp/ExchangeCalculateApp/Controllers/CalculatorViewController.swift
+++ ExchangeCalculateApp/ExchangeCalculateApp/Controllers/CalculatorViewController.swift
@@ -1,11 +1,17 @@
 import UIKit
 
-class CalculatorViewController: UIViewController {
+final class CalculatorViewController: UIViewController {
 
     private var item: ExchangeItem?
+    private let calculatorView = CalculatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        addViews()
+        configureLayout()
+        configureNavigtaioinBar()
+        bind()
     }
     
     convenience init(itme: ExchangeItem) {
@@ -21,4 +27,30 @@ class CalculatorViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func bind() {
+        guard let item else { return }
+        calculatorView.bind(model: item)
+    }
+    
+}
+// MARK: Add SubView, Configure UI,Layout
+private extension CalculatorViewController {
+    func addViews() {
+        view.addSubview(calculatorView)
+    }
+    func configureLayout() {
+        calculatorView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func configureNavigtaioinBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "환율 계산기"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
 }
