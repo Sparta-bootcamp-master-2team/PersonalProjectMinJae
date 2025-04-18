7299841656a1d28e47be4fb43c7687bb5f0fc804
minjae <qlqlzbto@naver.com>
2025-04-18 20:15:51 +0900

    feat: DTO, ViewModelProtocol 생성, NetworkManaget 디렉명 변경

---
+++ ExchangeCalculateApp/ExchangeCalculateApp/Network/ExchangeItemDTO.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct NetworkManagerDTO {
+    var items: [ExchangeItem]
+}
---
+++ ExchangeCalculateApp/ExchangeCalculateApp/ViewModel/ViewModelProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelProtocol.swift
+//  ExchangeCalculateApp
+//
+//  Created by main on 4/18/25.
+//
+
+import Foundation
