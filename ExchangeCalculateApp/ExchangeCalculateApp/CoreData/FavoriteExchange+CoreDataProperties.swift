1a63bf6cd414a526699fd417c6accda73b48f653
minjae <qlqlzbto@naver.com>
2025-04-24 01:24:16 +0900

    feat: 이전에 저장된 코어데이터 마이그레이션 작업을 위해 매핑모델 구현 및 커스텀 Policy 구현 /

--- ExchangeCalculateApp/ExchangeCalculateApp/CoreData/FavoriteExchange+CoreDataProperties.swift
+++ ExchangeCalculateApp/ExchangeCalculateApp/CoreData/FavoriteExchange+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  FavoriteExchange+CoreDataProperties.swift
 //  ExchangeCalculateApp
 //
-//  Created by main on 4/22/25.
+//  Created by main on 4/24/25.
 //
 //
 
--- ExchangeCalculateApp/ExchangeCalculateApp/CoreData/LastCurrency+CoreDataClass.swift
+++ ExchangeCalculateApp/ExchangeCalculateApp/CoreData/LastCurrency+CoreDataClass.swift
@@ -1,3 +1,11 @@
+//
+//  LastCurrency+CoreDataClass.swift
+//  ExchangeCalculateApp
+//
+//  Created by main on 4/24/25.
+//
+//
+
 import Foundation
 import CoreData
 
